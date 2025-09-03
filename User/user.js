d5693eb6d1d1173a97d9dae0104f25be7643799f
LeitanBogdan <bogdanprogramare2007@gmail.com>
2025-09-03 18:06:37 +0300

    login/signin -frontend

---
+++ User/user.js
@@ -0,0 +1,23 @@
+function signin(app, path, express){
+    app.get("/signin", function(req, res){
+        res.sendFile(path.join(__dirname, "Signin", "Signin.html"));
+    });
+
+    app.get("/signup", function(req, res){
+        res.sendFile(path.join(__dirname, "Signup", "signup.html"));
+    });
+
+    app.get("/Signin/Signin.css", function(req, res){
+        res.sendFile(path.join(__dirname, "Signin", "Signin.css"));
+    });
+
+    app.get("/Signup/signup.css", function(req, res){
+        res.sendFile(path.join(__dirname, "Signup", "signup.css"));
+    });
+
+    app.get("/Assets/background.png", function(req, res){
+        res.sendFile(path.join(__dirname, "Assets", "background.png"));
+    });
+}
+
+module.exports = { signin }
--- backend.js
+++ backend.js
@@ -1,79 +1,10 @@
 const express = require("express");
 const path = require("path");
-const mongodb = require("mongodb");
+let { signin } = require("./User/user")
 
 const app = express();
-const url = "mongodb://localhost:27017/";
-const client = new mongodb.MongoClient(url);
 
 //load Profiles
-app.get("/profiles", function(req, res){
-    res.sendFile(path.join(__dirname, "Profiles", "profiles.html"));
-});
-    
-app.get("/Profiles/assets/icon.png", function(req, res){
-    res.sendFile(path.join(__dirname, "Profiles", "assets", "icon.png"));
-});
-    
-app.get("/Profiles/profiles.css", function(req, res){
-    res.setHeader("Content-type", "text/css");
-    res.sendFile(path.join(__dirname, "Profiles", "profiles.css"));
-});
-    
-app.get("/Profiles/assets/newIcon.png", function(req, res){
-    res.sendFile(path.join(__dirname, "Profiles", "assets", "newIcon.png"));
-});
-    
-app.get("/Profiles/assets/logo.png", function(req, res){
-    res.sendFile(path.join(__dirname, "Profiles", "assets", "logo.png"));
-});
-
-//Letflix user
-app.get("/Moviepage/logo.png", function(req, res){
-    res.sendFile(path.join(__dirname, "Moviepage" , "image.png"));
-});
-
-app.get("/Moviepage/bar.css", function(req, res){
-    res.setHeader("Content-type", "text/css");
-    res.sendFile(path.join(__dirname, "Moviepage", "bar.css"));
-});
-
-//verify user
-app.get("/vireo/:id", conectDB, async function(req, res){
-    const exist = await validateUser(req.params.id);
-    if(exist){
-        res.sendFile(path.join(__dirname, "Moviepage", "moviepage.html"));
-    } else if(!exist){
-        res.send("No user found!");
-    }
-});
+signin(app, path, express);
 
 app.listen(5000);
-
-//DB operation
-async function conectDB(req, res, next){
-    try{
-        const connection = await client.connect();
-
-        if(connection){
-            next();
-        }
-
-    } catch(error){
-        console.log(error);
-        return res.sendFile(path.join(__dirname, "Errors", "dbNotConected.html"));
-    } 
-}
-
-async function validateUser(USER){
-    try{
-        const DB = client.db("vireo").collection("users");
-        const name = await DB.findOne({user: USER});
-
-        return !!name;
-    } catch(error){
-        console.log(error);
-    } finally{
-        await client.close();
-    }
-}
